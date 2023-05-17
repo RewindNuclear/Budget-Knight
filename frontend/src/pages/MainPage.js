157ede99d9c2c92de5b6797ac5a6c09aea65cb09
christian samson <79247120+RewindNuclear@users.noreply.github.com>
2023-05-17 12:37:49 -0400

    Create MainPage.js

---
+++ frontend/src/pages/MainPage.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useState } from 'react';
+
+import Transactions from '../components/Transactions';
+import NavBar from '../components/NavBarMain';
+import Goals from '../components/Goals';
+import SpendGraph from '../components/SpendGraph'
+import './mainpage.css'
+
+const MainPage = () =>
+{
+  // const [currentBalance, setCurrentBalance] = useState(0);
+  // const [graphUpdateFlag, setGraphUpdateFlag] = useState(false);
+
+  // const updateGraph = () => {
+  //   setGraphUpdateFlag(!graphUpdateFlag); // Toggle the flag to trigger an update
+  // };
+    return (
+        <div>
+          <NavBar />
+          <div className="container">
+            <div className="left-column" style={{border: '2px solid #AEDD97'}}>
+              <Transactions  />
+            </div>
+            <div className="right-column">
+              <div className="top-row" style={{border: '2px solid #AEDD97'}}>
+                <Goals />
+              </div>
+              <div className="bottom-row">
+                <SpendGraph />
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    
+};
+
+export default MainPage;
